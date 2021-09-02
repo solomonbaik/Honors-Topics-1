389a9f320f804a20af4052fc411b95e47add4646
2Hunter2 <53160039+2Hunter2@users.noreply.github.com>
2021-09-02 14:08:55 -0700

    Finished Project (#1)
    * Update ATM.java
    
    * Create .gitignore
    
    * check
    
    * WithdrawlMethod
    
    * CheckBalance Method
    
    * tester
    
    * Tester and toString in ATM CLass

--- src/ATM.java
+++ src/ATM.java
@@ -1,13 +1,51 @@
 import java.util.*;
 public class ATM {
-	
-	private static HashMap<String, Integer> map = new HashMap<String, Integer>();
-	
-	public void deposit(String bankAccountID, int amount) {
+
+	private static HashMap<String, Double> map = new HashMap<String, Double>();
+
+	public void deposit(String bankAccountID, double amount) {
 		if(!map.containsKey(bankAccountID)) {
 			map.put(bankAccountID, amount);
 		}else {
 			map.put(bankAccountID, map.get(bankAccountID) + amount);
 		}
 	}
+
+	public void withdraw (String bankAccountID, double amount) throws Exception
+	{
+		if (!map.containsKey(bankAccountID))
+		{
+			throw new Exception ();
+		}
+		else
+		{
+			if (amount <= map.get(bankAccountID))
+			{
+				map.put(bankAccountID, map.get(bankAccountID)-amount);
+			}
+		}
+	}
+
+	public String checkBalance (String bankAccountID) throws Exception
+	{
+		if (!map.containsKey(bankAccountID))
+		{
+			throw new Exception ();
+		}
+		else
+		{
+			return ("" + map.get(bankAccountID));
+		}
+	}
+
+	public String toString ()
+	{
+		String s = "";
+		for(HashMap.Entry<String,Double> account: map.entrySet())
+		{
+			s = s + account.getKey() + ": $" + account.getValue() + "\n";
+		}
+		return s;
+	}
 }
+
---
+++ src/ATMTester.java
@@ -0,0 +1,20 @@
+
+public class ATMTester {
+	public static void main (String [] args) throws Exception
+	{
+		ATM test = new ATM ();
+		test.deposit("Winfrey", 25159684372.43);
+		test.deposit("Bezos", 193495330293.43);
+		test.deposit("Bezos", 20394353.35);
+		test.withdraw("Bezos", 3593293.66);
+		test.deposit("MrTheiss",64033.55);
+		test.withdraw("MrTheiss", 443964.46);
+		
+		test.checkBalance("Bezos");
+		test.checkBalance("Winfrey");
+		//test.checkBalance("YoMama");
+		//test.checkBalance("MrTheiss");
+		
+		System.out.println (test.toString ());
+	}
+}
